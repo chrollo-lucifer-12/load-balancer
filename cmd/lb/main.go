dd4f7d14dd10f7764c7e773869a44385bbe5a9fa
sahil-singh1107 <sahilpython@gmail.com>
2026-08-23 21:50:35 +0530

    add tests for metrics

--- cmd/lb/main.go
+++ cmd/lb/main.go
@@ -1,22 +1,5 @@
 package main
 
-import (
-	"errors"
-	"time"
-)
-
-func unreliableService() (any, error) {
-
-	if time.Now().Unix()%2 == 0 {
-
-		return nil, errors.New("service failed")
-
-	}
-
-	return "Success!", nil
-
-}
-
 func main() {
 
 }
